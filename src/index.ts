db85afe8c65abcdc1d875eb15dafe29ef70fce45
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-01-14 23:44:20 +0800

    Add express.json()

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api", apiRouter);
 
 app.get("/", (req: Request, res: Response) => {
