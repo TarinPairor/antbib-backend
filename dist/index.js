29994cd8d877304e9b2939671844d79d8b7907ac
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-01-16 00:27:46 +0800

    Init supabase

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,14 +16,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const api_1 = __importDefault(require("./routes/api"));
+const supabase_js_1 = require("@supabase/supabase-js");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+const supabaseUrl = process.env.SUPABASE_URL || "";
+const supabaseKey = process.env.SUPABASE_KEY || "";
+const supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseKey);
 app.use(express_1.default.json());
 app.use("/api", api_1.default);
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
+app.get("/supabase_healthcheck", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { data, error } = yield supabase.from("test").select();
+    if (error) {
+        res.status(500).json({ error: error.message });
+        return;
+    }
+    res.status(200).json(data);
+}));
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 // src/index.ts
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+import { Request, Response } from "express-serve-static-core";
 import dotenv from "dotenv";
 import apiRouter from "./routes/api";
+import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const supabaseUrl = process.env.SUPABASE_URL || "";
+const supabaseKey = process.env.SUPABASE_KEY || "";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(express.json());
 
@@ -15,6 +21,18 @@ app.use("/api", apiRouter);
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+
+app.get(
+  "/supabase_healthcheck",
+  async (req: Request, res: Response): Promise<void> => {
+    const { data, error } = await supabase.from("test").select();
+    if (error) {
+      res.status(500).json({ error: error.message });
+      return;
+    }
+    res.status(200).json(data);
+  }
+);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
